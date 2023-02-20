f412b70ec30135a1723953e20f098aafbca34344
BirukG <byafet3@gmail.com>
2023-02-20 19:31:27 +0000

    webpack

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -2,18 +2,18 @@ import '../css/main.css';
 const $ = require('jquery');
 const _ = require('lodash');
 
-const counter = 0;
+let counter = 0;
 
-$("<div id='logo'></div>").appendTo('body');
+$("<div id='logo'> </div>").appendTo('body');
 $("<p>Holberton Dashboard</p>").appendTo('body');
 $("<p>Dashboard data for the students</p>").appendTo('body');
 $("<button>Click here to get started</button>").appendTo('body');
 $("<p id='count'></p>").appendTo('body');
 $("<p>Copyright - Holberton School</p>").appendTo('body');
 
-function updateCounter(){
-	counter++;
-  	$('#count').html(`${counter} clicks on the button`);
+function updateCounter() {
+  counter++;
+  $('#count').html(`${counter} clicks on the button`);
 }
 
 $("button").on("click", _.debounce(updateCounter, 500));
